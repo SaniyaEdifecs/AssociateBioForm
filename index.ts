55cb6e7ac68649fc2f27f5bc108bab40f0bd3f1f
SaniyaEdifecs <50659774+SaniyaEdifecs@users.noreply.github.com>
2020-02-19 16:49:09 +0530

    First commit

---
+++ index.ts
@@ -0,0 +1,65 @@
+// // A file is required to be in the root of the /src directory by the TypeScript compiler
+// import Reflect from 'core-js/es6/reflect';
+
+// if (!String.prototype.startsWith) {
+//     String.prototype.startsWith = function (search, pos) {
+//       return this.substr(!pos || pos < 0 ? 0 : +pos, search.length) === search
+//     }
+//   }
+  
+//   if (!Array.prototype.includes) {
+//     Array.prototype.includes = function (searchElement, ...args) {
+//       let O = Object(this)
+//       let len = parseInt(O.length, 10) || 0
+  
+//       if (len === 0) {
+//         return false
+//       }
+  
+//       let n = (args as any)[1] || 0
+//       let k
+  
+//       if (n >= 0) {
+//         k = n
+//       } else {
+//         k = len + n
+  
+//         if (k < 0) {
+//           k = 0
+//         }
+//       }
+  
+//       let currentElement
+  
+//       while (k < len) {
+//         currentElement = O[k]
+  
+//         if (searchElement === currentElement || (searchElement !== searchElement && currentElement !== currentElement)) {
+//           return true
+//         }
+  
+//         k++
+//       }
+  
+//       return false
+//     }
+//   }
+  
+//   if (!Object.values || !Object.entries) {
+//     const reduce = Function.bind.call(Function.call as any, Array.prototype.reduce)
+//     const isEnumerable = Function.bind.call(Function.call as any, Object.prototype.propertyIsEnumerable)
+//     const concat = Function.bind.call(Function.call as any, Array.prototype.concat)
+//     const keys = Reflect.ownKeys
+  
+//     if (!Object.values) {
+//       (Object.values as any) = function values (O: any) {
+//         return reduce(keys(O), (v: any, k: any) => concat(v, typeof k === 'string' && isEnumerable(O, k) ? [O[k]] : []), [])
+//       }
+//     }
+  
+//     if (!Object.entries) {
+//       (Object.entries as any) = function entries (O: any) {
+//         return reduce(keys(O), (e: any, k: any) => concat(e, typeof k === 'string' && isEnumerable(O, k) ? [[k, O[k]]] : []), [])
+//       }
+//     }
+//   }
---
+++ webparts/associateBioForm/components/AssociateBioForm.module.scss.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+require("./AssociateBioForm.module.css");
+const styles = {
+  associateBioForm: 'associateBioForm_6a52ceaf',
+  container: 'container_6a52ceaf',
+  row: 'row_6a52ceaf',
+  column: 'column_6a52ceaf',
+  'ms-Grid': 'ms-Grid_6a52ceaf',
+  title: 'title_6a52ceaf',
+  subTitle: 'subTitle_6a52ceaf',
+  description: 'description_6a52ceaf',
+  button: 'button_6a52ceaf',
+  label: 'label_6a52ceaf',
+  errorMessage: 'errorMessage_6a52ceaf'
+};
+
+export default styles;
+/* tslint:enable */
